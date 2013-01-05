17a250b7bf683f7a831979d1d9652d254b9adc01
Tom Emerson <tremerson@gmail.com>
2013-01-05 12:37:43 -0500

    Change the file suffix for SPARQL queries.

--- tree-semanticweb.el
+++ tree-semanticweb.el
@@ -2,15 +2,16 @@
 ;;;;
 ;;;; Configuration for semantic-web related modes
 
-;; sparql-mode isn't handled by any package managers, and I haven't setup GIT externals
-;; yet to pull the version I use, so you will need to grab it yourself from GitHub
+;; sparql-mode isn't handled by any package managers, and I haven't setup
+;; GIT externals yet to pull the version I use, so you will need to grab
+;; it yourself from GitHub
 ;;
 ;; https://github.com/candera/sparql-mode
 ;;
 ;; and put it in ~/emacs.
 
 (autoload 'sparql-mode "sparql-mode.el")
-(add-to-list 'auto-mode-alist '("\\.sparql\\'" . sparql-mode))
+(add-to-list 'auto-mode-alist '("\\.rq\\'" . sparql-mode))
 
 ;; ttl-mode isn't handled by any package managers and his hosted on BitBucket,
 ;; so you have to pull it yourself and put ttl-mode.el in ~/emacs.

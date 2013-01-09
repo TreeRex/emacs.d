f7f1e496fd5b50377b633349d4e368451882369b
Tom Emerson <tremerson@gmail.com>
2013-01-09 09:30:03 -0500

    Basic support for multiple local lisp implementations, though I haven't added SBCL yet

--- tree-lisp.el
+++ tree-lisp.el
@@ -22,8 +22,11 @@
 ;; Slime is setup via QuickLisp
 (setq slime-net-coding-system 'utf-8-unix)
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "/Users/temerson/bin/ccl -K utf-8"
-      common-lisp-hyperspec-root "file:///Users/temerson/Documents/HyperSpec/"
+
+(setq slime-lisp-implementations
+      '((ccl ("/Users/temerson/bin/ccl" "-K utf-8"))))
+
+(setq common-lisp-hyperspec-root "file:///Users/temerson/Documents/HyperSpec/"
       slime-startup-animation nil
       slime-complete-symbol-function 'slime-fuzzy-complete-symbol
       lisp-indent-function 'common-lisp-indent-function)

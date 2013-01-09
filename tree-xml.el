d20967d0cd3f01cf6a92a28f2d87afb63f06842c
Tom Emerson <tree@dreamersrealm.net>
2013-01-08 20:36:29 -0500

    Remove sparql-mode noise, since it moved to tree-semanticweb.el

--- tree-xml.el
+++ tree-xml.el
@@ -4,16 +4,6 @@
 
 (maybe-install-packages '(rnc-mode))
 
-;; sparql-mode isn't handled by any package managers, and I haven't setup GIT externals
-;; yet to pull the version I use, so you will need to grab it yourself from GitHub
-;;
-;; https://github.com/candera/sparql-mode
-;;
-;; and put it in ~/emacs.
-
-(autoload 'sparql-mode "sparql-mode.el")
-(add-to-list 'auto-mode-alist '("\\.rq\\'" . sparql-mode))
-
 (setq rnc-enable-imenu t)
 (setq rnc-enable-flymake t)
 (setq rnc-jing-jar-file "/Users/tree/src/jing-20091111/bin/jing.jar")
